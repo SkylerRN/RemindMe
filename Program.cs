9a59c7941097b7a4d9e11e27725e71cae290881d
SkylerRN <skylerruley123@gmail.com>
2023-03-24 10:06:56 -0400

    Added the loop allowing user to return to beginning if chosen.

--- Program.cs
+++ Program.cs
@@ -4,31 +4,61 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("How many Reminders would you like to add? ");
-        var numberOfRecords = int.Parse(Console.ReadLine());
-
-        var recordList = new List<Reminders>();
-        for (int i = 0; i < numberOfRecords; i++)
+        bool backTop = true;
+        while (backTop)
         {
-         //how it takes the information
-            var Reminders = new Reminders();
-            Console.WriteLine("Please enter a name for the reminder.");
-            Reminders.Name = Console.ReadLine();
-            Console.WriteLine("Please enter a date for " +  Reminders.Name );
-            Reminders.Date = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter a brief description for your reminder. ");
-            Reminders.Description = Console.ReadLine();
-            recordList.Add(Reminders);
-        }
-       //how it spits the information back out
-        foreach (var record in recordList)
-        {
-            Console.WriteLine($"Product:" + record.Name + " " + record.Date + " " + record.Description);
-        }
-      
-        Console.WriteLine("Please press 0 to exit or 1 to return to start.");
-        
+            Start:
+            Console.WriteLine("How many Reminders would you like to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
 
+            var recordList = new List<Reminders>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                //how it takes the information
+                var Reminders = new Reminders();
+                Console.WriteLine("Please enter a name for the reminder.");
+                Reminders.Name = Console.ReadLine();
+                Console.WriteLine("Please enter the day of the month " + Reminders.Name);
+                Reminders.Date = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter a brief description for your reminder. ");
+                Reminders.Description = Console.ReadLine();
+                recordList.Add(Reminders);
+            }
+            //how it spits the information back out
+            foreach (var record in recordList)
+            {
+                Console.WriteLine($"Reminder: we will remind you about " + record.Name + " on the " + record.Date + "th about " + record.Description);
+            }
+
+            Console.WriteLine("Please press 1 to exit or 2 to return to start.");
+            int choice = 0;
+
+            while (choice != 1)
+            {
+                Console.WriteLine("1. Quit");
+                Console.WriteLine("2. Return to start of program");
+                Console.Write("Enter your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Returning to main");
+                        goto  Start;
+
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        continue;
+                }
+            }
+        }
+                  backTop = false;
     }
 }
 
